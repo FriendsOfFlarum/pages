d8d3662b92b39ccdd139c597559b1283eed622fb
Clark Winkelmann <clark.winkelmann@gmail.com>
2020-02-26 01:31:11 +0100

    Add badges to draft and restricted pages in pages list

--- js/src/common/models/Page.js
+++ js/src/common/models/Page.js
@@ -1,7 +1,10 @@
+import app from 'flarum/app';
 import Model from 'flarum/Model';
 import mixin from 'flarum/utils/mixin';
 import computed from 'flarum/utils/computed';
 import { getPlainContent } from 'flarum/utils/string';
+import ItemList from 'flarum/utils/ItemList';
+import Badge from 'flarum/components/Badge';
 
 export default class Page extends mixin(Model, {
     title: Model.attribute('title'),
@@ -14,4 +17,26 @@ export default class Page extends mixin(Model, {
     isHidden: Model.attribute('isHidden'),
     isRestricted: Model.attribute('isRestricted'),
     isHtml: Model.attribute('isHtml'),
+
+    badges() {
+        const items = new ItemList();
+
+        if (this.isHidden()) {
+            items.add('hidden', Badge.component({
+                type: 'hidden',
+                icon: 'fas fa-trash',
+                label: app.translator.trans('fof-pages.admin.badges.hidden'),
+            }));
+        }
+
+        if (this.isRestricted()) {
+            items.add('restricted', Badge.component({
+                type: 'restricted',
+                icon: 'fas fa-user-shield',
+                label: app.translator.trans('fof-pages.admin.badges.restricted'),
+            }));
+        }
+
+        return items;
+    },
 }) {}
